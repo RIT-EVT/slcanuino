69d4e1752be85b7b1bf371e959998c0212c138a0
cbolles <collinbolles@gmail.com>
2022-02-17 13:03:29 -0500

    Reformat code

--- slcan/slcan.ino
+++ slcan/slcan.ino
@@ -2,7 +2,7 @@
 
 #define LED_OPEN 7
 #define LED_ERR 8
-#define CMD_LEN (sizeof("T12345678811223344556677881234\r")+1)
+#define CMD_LEN (sizeof("T12345678811223344556677881234\r") + 1)
 
 #define CANSPEED_500 500E3
 #define CANSPEED_100 100E3
@@ -10,58 +10,56 @@
 #define CANSPEED_1000 1000E3
 #define CANSPEED_250 250E3
 
-int g_can_speed = CANSPEED_500; // default: 500k
+int g_can_speed = CANSPEED_500;  // default: 500k
 int g_ts_en = 0;
 
-typedef struct
-{
-  uint16_t id;
-  uint16_t ide;
-  struct {
-    int8_t rtr : 1;
-    int8_t ide : 1;
-    uint8_t length : 4;
-  } header;
-  uint8_t data[8];
+typedef struct {
+    uint16_t id;
+    uint16_t ide;
+    struct {
+        int8_t rtr : 1;
+        int8_t ide : 1;
+        uint8_t length : 4;
+    } header;
+    uint8_t data[8];
 } tCAN;
 
 // transfer messages from CAN bus to host
-void xfer_can2tty(tCAN &msg)
-{
-  char buf[CMD_LEN];
-  int i;
-  static uint16_t ts = 0;
-  char *p;
-  uint8_t len;
+void xfer_can2tty(tCAN &msg) {
+    char buf[CMD_LEN];
+    int i;
+    static uint16_t ts = 0;
+    char *p;
+    uint8_t len;
 
     p = buf;
     if (msg.header.ide) {
-      if (msg.header.rtr) {
-        *p++ = 'R';
-      } else {
-        *p++ = 'T';
-      }
-      p += b2ahex(p, 4, 1, &msg.id);
-      p += b2ahex(p, 4, 1, &msg.ide);
-      len = msg.header.length % 10;
-      p += b2ahex(p, 1, 1, &len);
+        if (msg.header.rtr) {
+            *p++ = 'R';
+        } else {
+            *p++ = 'T';
+        }
+        p += b2ahex(p, 4, 1, &msg.id);
+        p += b2ahex(p, 4, 1, &msg.ide);
+        len = msg.header.length % 10;
+        p += b2ahex(p, 1, 1, &len);
     } else {
-      if (msg.header.rtr) {
-        *p++ = 'r';
-      } else {
-        *p++ = 't';
-      }
-      p += b2ahex(p, 3, 1, &msg.id);
-      len = msg.header.length;
-      p += b2ahex(p, 1, 1, &len);
+        if (msg.header.rtr) {
+            *p++ = 'r';
+        } else {
+            *p++ = 't';
+        }
+        p += b2ahex(p, 3, 1, &msg.id);
+        len = msg.header.length;
+        p += b2ahex(p, 1, 1, &len);
     }
 
     p += b2ahex(p, 2, msg.header.length, msg.data);
 
     // insert timestamp if needed
     if (g_ts_en) {
-      p += b2ahex(p, 4, 1, &ts); // up to 60,000ms
-      ts++;
+        p += b2ahex(p, 4, 1, &ts);  // up to 60,000ms
+        ts++;
     }
 
     *p++ = '\r';
@@ -70,284 +68,275 @@ void xfer_can2tty(tCAN &msg)
 }
 
 void onReceive(int packetSize) {
-  tCAN msg;
+    tCAN msg;
 
-  msg.id = CAN.packetId();
-  msg.header.length = packetSize;
-  msg.header.ide = CAN.packetExtended();
-  msg.header.rtr = CAN.packetRtr();
+    msg.id = CAN.packetId();
+    msg.header.length = packetSize;
+    msg.header.ide = CAN.packetExtended();
+    msg.header.rtr = CAN.packetRtr();
 
-  for(int i = 0; i < packetSize; i++) {
-    msg.data[i] = CAN.read();
-  }
+    for (int i = 0; i < packetSize; i++) {
+        msg.data[i] = CAN.read();
+    }
 
-  xfer_can2tty(msg);
+    xfer_can2tty(msg);
 }
 
-void transmit(tCAN& msg) {
-  CAN.beginPacket(msg.id);
+void transmit(tCAN &msg) {
+    CAN.beginPacket(msg.id);
 
-  for(size_t i = 0; i < msg.header.length; i++) {
-    CAN.write(msg.data[i]);
-  }
+    for (size_t i = 0; i < msg.header.length; i++) {
+        CAN.write(msg.data[i]);
+    }
 
-  CAN.endPacket();
+    CAN.endPacket();
 }
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  pinMode(LED_OPEN, OUTPUT);
-  pinMode(LED_ERR, OUTPUT);
-  Serial.begin(115200); // select from 115200,500000,1000000,2000000
-    // start the CAN bus at 500 kbps
-  if (!CAN.begin(500E3)) {
-    //Serial.println("Starting CAN failed!");
-    while (1);
-  }
-  CAN.onReceive(onReceive);
+    pinMode(LED_OPEN, OUTPUT);
+    pinMode(LED_ERR, OUTPUT);
+    Serial.begin(115200);  // select from 115200,500000,1000000,2000000
+                           // start the CAN bus at 500 kbps
+    if (!CAN.begin(500E3)) {
+        // Serial.println("Starting CAN failed!");
+        while (1)
+            ;
+    }
+    CAN.onReceive(onReceive);
 }
 
-int b2ahex(char *p, uint8_t s, uint8_t n, void *v)
-{
-  const char *hex = "0123456789ABCDEF";
+int b2ahex(char *p, uint8_t s, uint8_t n, void *v) {
+    const char *hex = "0123456789ABCDEF";
 
-  if (s == 1) {
-    uint8_t *tmp = (uint8_t *)v;
-    for (int i=0; i<n; i++) {
-      *p++ = hex[tmp[i] & 0x0f];
+    if (s == 1) {
+        uint8_t *tmp = (uint8_t *)v;
+        for (int i = 0; i < n; i++) {
+            *p++ = hex[tmp[i] & 0x0f];
+        }
+    } else if (s == 2) {
+        uint8_t *tmp = (uint8_t *)v;
+        for (int i = 0; i < n; i++) {
+            *p++ = hex[(tmp[i] >> 4) & 0x0f];
+            *p++ = hex[tmp[i] & 0x0f];
+        }
+    } else if (s == 3) {
+        uint16_t *tmp = (uint16_t *)v;
+        for (int i = 0; i < n; i++) {
+            *p++ = hex[(tmp[i] >> 8) & 0x0f];
+            *p++ = hex[(tmp[i] >> 4) & 0x0f];
+            *p++ = hex[tmp[i] & 0x0f];
+        }
+    } else if (s == 4) {
+        uint16_t *tmp = (uint16_t *)v;
+        for (int i = 0; i < n; i++) {
+            *p++ = hex[(tmp[i] >> 12) & 0x0f];
+            *p++ = hex[(tmp[i] >> 8) & 0x0f];
+            *p++ = hex[(tmp[i] >> 4) & 0x0f];
+            *p++ = hex[tmp[i] & 0x0f];
+        }
     }
-  } else if (s == 2) {
-    uint8_t *tmp = (uint8_t *)v;
-    for (int i=0; i<n; i++) {
-      *p++ = hex[(tmp[i] >> 4) & 0x0f];
-      *p++ = hex[tmp[i] & 0x0f];
-    }
-  } else if (s == 3) {
-    uint16_t *tmp = (uint16_t *)v;
-    for (int i=0; i<n; i++) {
-      *p++ = hex[(tmp[i] >> 8) & 0x0f];
-      *p++ = hex[(tmp[i] >> 4) & 0x0f];
-      *p++ = hex[tmp[i] & 0x0f];
-    }
-  } else if (s == 4) {
-    uint16_t *tmp = (uint16_t *)v;
-    for (int i=0; i<n; i++) {
-      *p++ = hex[(tmp[i] >> 12) & 0x0f];
-      *p++ = hex[(tmp[i] >> 8) & 0x0f];
-      *p++ = hex[(tmp[i] >> 4) & 0x0f];
-      *p++ = hex[tmp[i] & 0x0f];
-    }
-  }
-  return s*n;
+    return s * n;
 }
 
-int a2bhex_sub(char a)
-{
-  int val = 0;
+int a2bhex_sub(char a) {
+    int val = 0;
 
-  if ('0' <= a && a <= '9') {
-    val = a - '0';
-  } else if ('A' <= a && a <= 'F') {
-    val = a - 'A' + 10;
-  } else if ('a' <= a && a <= 'f') {
-    val = a - 'a' + 10;
-  }
-  return val;
-}
-
-int a2bhex(char *p, uint8_t s, uint8_t n, void *v)
-{
-  int i, j;
-  char buf[4+1];
-  int val;
-
-  if (s == 1 || s == 2)  {
-    uint8_t *tmp = (uint8_t *)v;
-    for (i=0; i<n; i++) {
-      val = 0;
-      for (j=0; j<s; j++) {
-        val = (val << 4) | a2bhex_sub(*p++);
-      }
-      *tmp++ = val;
+    if ('0' <= a && a <= '9') {
+        val = a - '0';
+    } else if ('A' <= a && a <= 'F') {
+        val = a - 'A' + 10;
+    } else if ('a' <= a && a <= 'f') {
+        val = a - 'a' + 10;
     }
-  } else if (s == 3 || s == 4) {
-    uint16_t *tmp = (uint16_t *)v;
-    for (i=0; i<n; i++) {
-      val = 0;
-      for (j=0; j<s; j++) {
-        val = (val << 4) | a2bhex_sub(*p++);
-      }
-      *tmp++ = val;
+    return val;
+}
+
+int a2bhex(char *p, uint8_t s, uint8_t n, void *v) {
+    int i, j;
+    char buf[4 + 1];
+    int val;
+
+    if (s == 1 || s == 2) {
+        uint8_t *tmp = (uint8_t *)v;
+        for (i = 0; i < n; i++) {
+            val = 0;
+            for (j = 0; j < s; j++) {
+                val = (val << 4) | a2bhex_sub(*p++);
+            }
+            *tmp++ = val;
+        }
+    } else if (s == 3 || s == 4) {
+        uint16_t *tmp = (uint16_t *)v;
+        for (i = 0; i < n; i++) {
+            val = 0;
+            for (j = 0; j < s; j++) {
+                val = (val << 4) | a2bhex_sub(*p++);
+            }
+            *tmp++ = val;
+        }
     }
-  }
-  return n;
+    return n;
 }
 
-void slcan_ack()
-{
-  Serial.write('\r'); // ACK
+void slcan_ack() {
+    Serial.write('\r');  // ACK
 }
 
-void slcan_nack()
-{
-  Serial.write('\a'); // NACK
+void slcan_nack() {
+    Serial.write('\a');  // NACK
 }
 
-void send_canmsg(char *buf)
-{
-  tCAN msg;
-  int len = strlen(buf) - 1;
-  uint16_t id[2];
-  uint8_t hlen;
-  int is_eff = buf[0] & 0x20 ? 0 : 1;
-  int is_rtr = buf[0] & 0x02 ? 1 : 0;
+void send_canmsg(char *buf) {
+    tCAN msg;
+    int len = strlen(buf) - 1;
+    uint16_t id[2];
+    uint8_t hlen;
+    int is_eff = buf[0] & 0x20 ? 0 : 1;
+    int is_rtr = buf[0] & 0x02 ? 1 : 0;
 
-  if (!is_eff && len >= 4) { // SFF
-    a2bhex(&buf[1], 3, 1, id);
-    msg.id = id[0];
-    msg.header.rtr = is_rtr;
-    msg.header.ide = 0;
-    a2bhex(&buf[4], 1, 1, &hlen);
-    msg.header.length = hlen;
-    if (len - 4 - 1 == msg.header.length * 2) {
-      a2bhex(&buf[5], 2, msg.header.length, msg.data);
-      transmit(msg);
+    if (!is_eff && len >= 4) {  // SFF
+        a2bhex(&buf[1], 3, 1, id);
+        msg.id = id[0];
+        msg.header.rtr = is_rtr;
+        msg.header.ide = 0;
+        a2bhex(&buf[4], 1, 1, &hlen);
+        msg.header.length = hlen;
+        if (len - 4 - 1 == msg.header.length * 2) {
+            a2bhex(&buf[5], 2, msg.header.length, msg.data);
+            transmit(msg);
+        }
+
+    } else if (is_eff && len >= 9) {  // EFF
+        a2bhex(&buf[1], 4, 2, id);
+        msg.id = id[0] & 0x1fff;
+        msg.ide = id[1];
+        msg.header.rtr = is_rtr;
+        msg.header.ide = 1;
+        a2bhex(&buf[9], 1, 1, &hlen);
+        msg.header.length = hlen;
+        if (len - 9 - 1 == msg.header.length * 2) {
+            a2bhex(&buf[10], 2, msg.header.length, msg.data);
+            transmit(msg);
+        }
     }
-
-  } else if (is_eff && len >= 9) { // EFF
-    a2bhex(&buf[1], 4, 2, id);
-    msg.id = id[0] & 0x1fff;
-    msg.ide = id[1];
-    msg.header.rtr = is_rtr;
-    msg.header.ide = 1;
-    a2bhex(&buf[9], 1, 1, &hlen);
-    msg.header.length = hlen;
-    if (len - 9 - 1 == msg.header.length * 2) {
-      a2bhex(&buf[10], 2, msg.header.length, msg.data);
-      transmit(msg);
-    }
-  }
 }
 
-void pars_slcancmd(char *buf)
-{
-  switch (buf[0]) {
-    // common commands
-    case 'O': // open channel
-      digitalWrite(LED_OPEN, HIGH);
-      slcan_ack();
-      break;
-    case 'C': // close channel
-      digitalWrite(LED_OPEN, LOW);
-      digitalWrite(LED_ERR, LOW);
-      slcan_ack();
-      break;
-    case 't': // SFF
-    case 'T': // EFF
-    case 'r': // RTR/SFF
-    case 'R': // RTR/EFF
-      send_canmsg(buf);
-      slcan_ack();
-      break;
-    case 'Z': // turn timestamp on/off
-      if (buf[1] == '0') {
-        g_ts_en = 0;
-      } else if (buf[1] == '1') {
-        g_ts_en = 1;
-      } else {
-        slcan_nack();
-      }
-      slcan_ack();
-      break;
-    case 'M': // acceptance mask
-      slcan_ack();
-      break;
-    case 'm': // acceptance value
-      slcan_ack();
-      break;
+void pars_slcancmd(char *buf) {
+    switch (buf[0]) {
+        // common commands
+        case 'O':  // open channel
+            digitalWrite(LED_OPEN, HIGH);
+            slcan_ack();
+            break;
+        case 'C':  // close channel
+            digitalWrite(LED_OPEN, LOW);
+            digitalWrite(LED_ERR, LOW);
+            slcan_ack();
+            break;
+        case 't':  // SFF
+        case 'T':  // EFF
+        case 'r':  // RTR/SFF
+        case 'R':  // RTR/EFF
+            send_canmsg(buf);
+            slcan_ack();
+            break;
+        case 'Z':  // turn timestamp on/off
+            if (buf[1] == '0') {
+                g_ts_en = 0;
+            } else if (buf[1] == '1') {
+                g_ts_en = 1;
+            } else {
+                slcan_nack();
+            }
+            slcan_ack();
+            break;
+        case 'M':  // acceptance mask
+            slcan_ack();
+            break;
+        case 'm':  // acceptance value
+            slcan_ack();
+            break;
 
-    // non-standard commands
-    case 'S': // setup CAN bit-rates
-      switch (buf[1]) {
-        case '0': // 10k
-        case '1': // 20k
-        case '2': // 50k
-          slcan_nack();
-          break;
-        case '3': // 100k
-          g_can_speed = CANSPEED_100;
-          slcan_ack();
-          break;
-        case '4': // 125k
-          g_can_speed = CANSPEED_125;
-          slcan_ack();
-          break;
-        case '5': // 250k
-          g_can_speed = CANSPEED_250;
-          slcan_ack();
-          break;
-        case '6': // 500k
-          g_can_speed = CANSPEED_500;
-          slcan_ack();
-          break;
-        case '7': // 800k
-          slcan_nack();
-          break;
-        case '8': // 1000k
-          g_can_speed = CANSPEED_1000;
-          slcan_ack();
-          break;
-        default:
-          slcan_nack();
-          break;
-      }
-      break;
-    case 's': // directly set bitrate register of mcp2515
-      slcan_nack();
-      break;
-    case 'F': // status flag
-      Serial.print("F12");
-      slcan_ack();
-      break;
-    case 'V': // hw/sw version
-      Serial.print("V1234");
-      slcan_ack();
-      break;
-    case 'N': // serial number
-      Serial.print("N1234");
-      slcan_ack();
-      break;
-    default: // unknown command
-      slcan_nack();
-      break;
-  }
+        // non-standard commands
+        case 'S':  // setup CAN bit-rates
+            switch (buf[1]) {
+                case '0':  // 10k
+                case '1':  // 20k
+                case '2':  // 50k
+                    slcan_nack();
+                    break;
+                case '3':  // 100k
+                    g_can_speed = CANSPEED_100;
+                    slcan_ack();
+                    break;
+                case '4':  // 125k
+                    g_can_speed = CANSPEED_125;
+                    slcan_ack();
+                    break;
+                case '5':  // 250k
+                    g_can_speed = CANSPEED_250;
+                    slcan_ack();
+                    break;
+                case '6':  // 500k
+                    g_can_speed = CANSPEED_500;
+                    slcan_ack();
+                    break;
+                case '7':  // 800k
+                    slcan_nack();
+                    break;
+                case '8':  // 1000k
+                    g_can_speed = CANSPEED_1000;
+                    slcan_ack();
+                    break;
+                default:
+                    slcan_nack();
+                    break;
+            }
+            break;
+        case 's':  // directly set bitrate register of mcp2515
+            slcan_nack();
+            break;
+        case 'F':  // status flag
+            Serial.print("F12");
+            slcan_ack();
+            break;
+        case 'V':  // hw/sw version
+            Serial.print("V1234");
+            slcan_ack();
+            break;
+        case 'N':  // serial number
+            Serial.print("N1234");
+            slcan_ack();
+            break;
+        default:  // unknown command
+            slcan_nack();
+            break;
+    }
 }
 
 // transfer messages from host to CAN bus
-void xfer_tty2can()
-{
-  int length;
-  static char cmdbuf[CMD_LEN];
-  static int cmdidx = 0;
+void xfer_tty2can() {
+    int length;
+    static char cmdbuf[CMD_LEN];
+    static int cmdidx = 0;
 
-  if ((length = Serial.available()) > 0) {
-    for (int i = 0; i < length; i++) {
-      char val = Serial.read();
-      cmdbuf[cmdidx++] = val;
+    if ((length = Serial.available()) > 0) {
+        for (int i = 0; i < length; i++) {
+            char val = Serial.read();
+            cmdbuf[cmdidx++] = val;
 
-      if (cmdidx == CMD_LEN) { // command is too long
-        slcan_nack();
-        cmdidx = 0;
-      } else if (val == '\r') { // end of command
-        cmdbuf[cmdidx] = '\0';
-        pars_slcancmd(cmdbuf);
-        cmdidx = 0;
-      }
+            if (cmdidx == CMD_LEN) {  // command is too long
+                slcan_nack();
+                cmdidx = 0;
+            } else if (val == '\r') {  // end of command
+                cmdbuf[cmdidx] = '\0';
+                pars_slcancmd(cmdbuf);
+                cmdidx = 0;
+            }
+        }
     }
-  }
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  xfer_tty2can();
-}
+void loop() { xfer_tty2can(); }
